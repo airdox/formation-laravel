798c95dd1577cbb02a392eaa0689e13b00f0c368
Kevin Canico <kevcanico25@gmail.com>
2022-09-24 16:54:06 +0200

    Add hello guys

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
+| Hello Guys
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
